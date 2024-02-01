e76813db23eedda4f5c0478cbcd6629fc7a37c47
Paul Cyrocki <itsbiggs@gmail.com>
2024-02-01 10:39:28 -0500

    Initial Commit

--- main.cpp
+++ main.cpp
@@ -7,23 +7,27 @@ std::string targetScale;
 
 
 double convertTemperature(double temperature, std::string currentScale, std::string targetScale) {
-    //conversion formula for C to F
-if (currentScale == "C" && targetScale == "F") {
-    return (9.0 / 5.0) * (temperature + 32);
     // convert F to C
-} else if (currentScale == "F" && targetScale == "C") {
+if (currentScale == "F" && targetScale == "C") {
     return (5.0 / 9.0) * (temperature - 32);
+    // convert F to K
 } else if (currentScale == "F" && targetScale == "K") {
     return  (temperature -32) * 5.0/9.0 + 273.15;
+    // convert K to F
 } else if (currentScale == "K" && targetScale == "F") {
     return ( temperature - 273.15) * (9.0 / 5.0) + 32;
+    // convert C to K
 } else if (currentScale == "C" && targetScale == "K") {
     return ( temperature + 273.15);
+    //convert K to C
 } else if (currentScale == "K" && targetScale == "C") {
     return (temperature - 273.15);
+     //convert C to F
+} else if (currentScale == "C" && targetScale == "F") {
+    return (9.0 / 5.0) * (temperature + 32);
     // else throw error
 } else {
-    throw std::invalid_argument("Unsupported temperature scale.  Please use C or F");
+    throw std::invalid_argument("Unsupported temperature scale.  Please use C, F, or K");
 }
 }
 
